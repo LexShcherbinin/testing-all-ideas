cd82ed18ce694d688ced8acb7112624425ef0c5c
LexPalych <lexshcherbinin@gmail.com>
2023-07-27 19:25:39 +0300

    MVC-Дополнил FirstController

--- src/main/java/spring/firstmvc/controllers/FirstController.java
+++ src/main/java/spring/firstmvc/controllers/FirstController.java
@@ -1,9 +1,14 @@
 package spring.firstmvc.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+//@RequestMapping("/api")
 public class FirstController {
 
   @GetMapping("/hello")
@@ -15,5 +20,26 @@ public class FirstController {
   public String goodByePage() {
     return "first/goodbye";
   }
+
+  @GetMapping("/hello-with-params")
+  public String method1(
+      @RequestParam(value = "name", required = false) String name,
+      @RequestParam(value = "surname", required = false) String surname,
+      Model model
+  ) {
+    String text = String.format("Hello, %s %s\n", name, surname);
+
+//    System.out.println(text);
+
+    model.addAttribute("message", text);
+    return "first/hello";
+  }
+
+//  @GetMapping("/hello-with-params")
+//  public String method2(HttpServletRequest httpServletRequest) {
+//
+//    System.out.printf("Hello, %s %s\n", httpServletRequest.getParameter("name"), httpServletRequest.getParameter("surname"));
+//    return "first/hello";
+//  }
 
 }
