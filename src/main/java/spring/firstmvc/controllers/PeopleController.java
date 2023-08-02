558684929f102c40e32ed7f784fe1dc7b58e9ec1
LexPalych <lexshcherbinin@gmail.com>
2023-08-02 20:25:07 +0300

    MVC-Добавил страницу new.html

--- src/main/java/spring/firstmvc/controllers/PeopleController.java
+++ src/main/java/spring/firstmvc/controllers/PeopleController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import spring.firstmvc.dao.PersonDAO;
+import spring.firstmvc.model.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -30,4 +33,22 @@ public class PeopleController {
     model.addAttribute("person", personDAO.show(id));
     return "people/show";
   }
+
+  @GetMapping("/new")
+  public String newPerson(Model model) {
+    model.addAttribute("person", new Person());
+    return "people/new";
+  }
+
+//  @GetMapping("/new")
+//  public String newPerson(@ModelAttribute("person") Person person) {
+//    return "people/new";
+//  }
+
+  @PostMapping()
+  public String create(@ModelAttribute("person") Person person) {
+    personDAO.save(person);
+    return "redirect:/people";
+  }
+
 }
