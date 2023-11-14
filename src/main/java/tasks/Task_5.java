fb162961208eacbcfc01600438413352e061a90b
lexsh <Aleksandr.Scherbinin@mvideo.ru>
2023-11-14 18:09:25 +0300

    Поправил Task_5

--- src/main/java/tasks/Task_5.java
+++ src/main/java/tasks/Task_5.java
@@ -1,91 +1,75 @@
 package tasks;
 
-import static tasks.Task_5.BanknoteDenomination.BD_10;
-import static tasks.Task_5.BanknoteDenomination.BD_5000;
-import static tasks.Task_5.BanknoteDenomination.getNext;
-
 import java.util.ArrayList;
 import java.util.List;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.experimental.Accessors;
+import java.util.Map;
 
 /**
  * Банкомат. Имеется сумма, которую банкомат должен выдать, и набор купюр заданных номиналов: {10, 50, 100, 200, 500, 1000, 2000, 5000}.
  * <p>
- * Количество купюр каждого номинала не ограничено. Требуется получить заданную сумму минимальным количеством купюр.
+ * Количество купюр каждого номинала не ограничено. Требуется получить заданную сумму набором купюр большего номинала.
  * <p>
  * Пример: 7830 рублей -> 1х 5000р. + 2х 2000р. + 1х 500р. + 3х 100р. + 3х + 10р.
  */
 public class Task_5 {
 
   public static void main(String[] args) {
-    int moneyAmount = 7830;
-    List<BanknoteDenomination> banknoteMap = qqq(moneyAmount, BD_5000);
+    int moneyAmount = 17850;
+    List<Map<Integer, Integer>> banknoteMap = getBanknotes(moneyAmount, BanknoteDenomination.BD_5000);
 
     banknoteMap.forEach(banknote -> {
-      System.out.printf("%s x %s\n", banknote.getCount(), banknote.getDenomination());
+      for (int denomination : banknote.keySet()) {
+        System.out.printf("%s x %s\n", banknote.get(denomination), denomination);
+      }
     });
   }
 
-  private static List<BanknoteDenomination> qqq(int moneyAmount, int banknote) {
-    List<BanknoteDenomination> list = new ArrayList<>();
-
-    int count = moneyAmount / banknote;
-    int residual = moneyAmount % banknote;
-
-    if (banknote == BD_10) {
-      BanknoteDenomination banknoteDenomination = new BanknoteDenomination()
-          .setCount(count)
-          .setResidual(0)
-          .setDenomination(banknote);
-
-      return List.of(banknoteDenomination);
+  private static List<Map<Integer, Integer>> getBanknotes(int moneyAmount, int banknoteDenomination) {
+    if (moneyAmount % 10 != 0) {
+      throw new IllegalArgumentException("Данную сумму банкомат выдать не может");
     }
 
+    int count = moneyAmount / banknoteDenomination;
+    int residual = moneyAmount % banknoteDenomination;
+
+    if (banknoteDenomination == BanknoteDenomination.BD_10) {
+      return List.of(Map.of(banknoteDenomination, count));
+    }
+
+    List<Map<Integer, Integer>> list = new ArrayList<>();
+
     if (count == 0) {
-      list.addAll(qqq(moneyAmount, getNext(banknote)));
+      list.addAll(getBanknotes(moneyAmount, BanknoteDenomination.getNextDenominate(banknoteDenomination)));
 
     } else {
-      BanknoteDenomination banknoteDenomination = new BanknoteDenomination()
-          .setCount(count)
-          .setResidual(residual)
-          .setDenomination(banknote);
-
-      list.add(banknoteDenomination);
+      list.add(Map.of(banknoteDenomination, count));
 
       if (residual != 0) {
-        list.addAll(qqq(residual, getNext(banknote)));
+        list.addAll(getBanknotes(residual, BanknoteDenomination.getNextDenominate(banknoteDenomination)));
       }
     }
 
     return list;
   }
 
-  @Getter
-  @Setter
-  @Accessors(chain = true)
-  public static class BanknoteDenomination {
+  private static final class BanknoteDenomination {
 
-    private int count;
-    private int residual;
-    private int denomination;
+    private static final int BD_10 = 10;
+    private static final int BD_50 = 50;
+    private static final int BD_100 = 100;
+    private static final int BD_200 = 200;
+    private static final int BD_500 = 500;
+    private static final int BD_1000 = 1000;
+    private static final int BD_2000 = 2000;
+    private static final int BD_5000 = 5000;
 
-    public static final int BD_10 = 10;
-    public static final int BD_50 = 50;
-    public static final int BD_100 = 100;
-    public static final int BD_200 = 200;
-    public static final int BD_500 = 500;
-    public static final int BD_1000 = 1000;
-    public static final int BD_2000 = 2000;
-    public static final int BD_5000 = 5000;
-
-    public static Integer getNext(int banknote) {
+    public static Integer getNextDenominate(int banknote) {
       return switch (banknote) {
         case BD_5000 -> BD_2000;
         case BD_2000 -> BD_1000;
         case BD_1000 -> BD_500;
-        case BD_500 -> BD_100;
+        case BD_500 -> BD_200;
+        case BD_200 -> BD_100;
         case BD_100 -> BD_50;
         case BD_50 -> BD_10;
         case BD_10 -> null;
